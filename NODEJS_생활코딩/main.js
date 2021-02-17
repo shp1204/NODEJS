e750803e9bef65ac4ac26bae294b288d874c27cd
shp1204 <5000roto@naver.com>
2021-02-17 11:12:17 +0900

    생활코딩_tutorial 완료

--- NODEJS_생활코딩/main.js
+++ NODEJS_생활코딩/main.js
@@ -2,35 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
- 
-function templateHTML(title, list, body){
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    <a href="/create">create</a>
-    ${body}
-  </body>
-  </html>
-  `;
-}
-function templateList(filelist){
-  var list = '<ul>';
-  var i = 0;
-  while(i < filelist.length){
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    i = i + 1;
-  }
-  list = list+'</ul>';
-  return list;
-}
- 
+var template = require('/lib/template.js');
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -40,28 +13,39 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var list = template.list(filelist);
+          var html = template.html(title, list, 
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`);
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         })
       } else {
         fs.readdir('./data', function(error, filelist){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            var title = queryData.id; // onsubmit="check"
+            var list = template.list(filelist);
+            var html = template.html(title, list, 
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a>
+              <a href="/update?id=${title}">update</a>
+              <form action = "delete_process" method="post">
+                <input type = "hidden" name = "id" value = "${title}">
+                <input type = "submit" value = "delete">
+              </form>`);
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
+
+            
           });
         });
       }
     } else if(pathname === '/create'){
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `
-        <form action = "http://localhost:3000/create_process" method="post">
+        var list = template.list(filelist);
+        var html = template.html(title, list, `
+        <form action = "/create_process" method="post">
         <p><input type = "text" name = "title" placeholder = "title"></p>
         <p>
             <textarea name = "description" placeholder = "description"></textarea>
@@ -69,9 +53,10 @@ var app = http.createServer(function(request,response){
         <p>
             <input type="submit">
         </p>
-    </form>`,'');
+    </form>
+    `, '');
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
     });
   } else if(pathname === '/create_process'){
     var body = '';
@@ -85,13 +70,71 @@ var app = http.createServer(function(request,response){
       // console.log(post);
       // 파일 저장해주자
       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        
         // 사용자가 자기가 쓴 글이 잘 전송되었는지 확인(redirection)
         response.writeHead(302, {Location : `/?id=${qs.escape(title)}`});
         response.end();
       })
     });
-  } else {
+  } else if(pathname === '/update'){
+    fs.readdir('./data', function(error, filelist){
+      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var title = queryData.id;
+        var list = template.list(filelist);
+        var html = template.html(title, list, 
+          `
+          <form action = "/update_process" method="post">
+          <input type="hidden" name="id" value="${title}"> 
+        <p><input type = "text" name = "title" placeholder = "title" value = "${title}"></p>
+        <p>
+            <textarea name = "description" placeholder = "description">${description}</textarea>
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+    </form>
+          
+          `,
+          `<a href="/create">create</a><a href="/update?id=${title}">update</a>`);
+        response.writeHead(200);
+        response.end(html);
+      });
+    });
+  } else if(pathname === '/update_process'){
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var title = post.title;
+      var description= post.description;
+      fs.rename(`data/${id}`, `data/${title}`, function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        
+          // 사용자가 자기가 쓴 글이 잘 전송되었는지 확인(redirection)
+          response.writeHead(302, {Location : `/?id=${qs.escape(title)}`});
+          response.end();
+      })
+      // console.log(post);
+      // 파일 저장해주자
+      })
+    });
+  } else if(pathname === '/delete_process'){
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function(error){
+        response.writeHead(302, {Location : `/`});
+        response.end();
+      })
+    });
+  }
+  else {
       response.writeHead(404);
       response.end('Not found');
     }
