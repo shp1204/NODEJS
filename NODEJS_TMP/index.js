393e290726002c089613d85e0b0d3a247811c3e1
shp1204 <5000roto@naver.com>
2021-02-17 14:36:35 +0900

    데이터 DB로 전송하기 완료

--- NODEJS_TMP/index.js
+++ NODEJS_TMP/index.js
@@ -1,7 +1,17 @@
+
 const mdbConn = require('./mariaDBConn.js')
 const express = require('express');
+const { connectionLimit } = require('./consts.js');
 const app = express();
- 
+
+// insert
+mdbConn.insertUserData()
+.then((rows) => {
+  console.log("성공!");
+})
+
+
+// read
 mdbConn.getUserList()
   .then((rows) => {
     console.log(rows);
@@ -9,8 +19,10 @@ mdbConn.getUserList()
   .catch((errMsg) => {
     console.log(errMsg);
   });
- 
+
+
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
--- NODEJS_TMP/mariaDBConn.js
+++ NODEJS_TMP/mariaDBConn.js
@@ -4,15 +4,48 @@ const vals = require('./consts.js');
 const pool = mariadb.createPool({
     host: vals.DBHost, port:vals.DBPort,
     user: vals.DBUser, password: vals.DBPass,
-    connectionLimit: 5
+    connectionLimit: 5, database: vals.DB
 });
+
+async function insertUserData(){
+    let conn;
+    try{
+        conn = await pool.getConnection();
+        const info = {
+            "name": 'Jiwon',
+            "email": 'jiwon@mail.com',
+            "age" : '22'
+        };
+
+        // insert using SQL
+        // const result = await 
+        var sql = 'INSERT INTO users2(name, email, age) VALUES(?,?,?)';
+        var params = [info['name'], info['email'], info['age']]
+        conn.query(sql, params, function(err, rows, fields){
+            if(err){
+                console.log(err);
+            } else{
+                console.log(rows.name);
+            }
+        });
+    }
+    catch(err){
+        throw err;
+    }
+    finally{
+        if (conn) conn.release();
+    }
+    
+}
  
 async function GetUserList(){
     let conn, rows;
     try{
         conn = await pool.getConnection();
-        conn.query('USE nodejs_test');
-        rows = await conn.query('SELECT * FROM users');
+
+        conn.query('USE nodejs_test'); // database
+
+        rows = await conn.query('SELECT * FROM users2');
     }
     catch(err){
         throw err;
@@ -24,5 +57,6 @@ async function GetUserList(){
 }
  
 module.exports = {
+    insertUserData : insertUserData,
     getUserList: GetUserList
 }
